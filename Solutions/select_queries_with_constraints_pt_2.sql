031b475318308695dd5676d161ec32ccd85d7347
Orange9000 <rollingonlinoleumfarting@gmail.com>
2017-05-05 09:43:09 +0300

    Create select_queries_with_constraints_pt_2.sql

---
+++ Solutions/select_queries_with_constraints_pt_2.sql
@@ -0,0 +1,4 @@
+SELECT title FROM Movies WHERE title LIKE "%Toy story%"
+SELECT title from Movies WHERE Director = "John Lasseter"
+SELECT title from Movies WHERE Director  != "John Lasseter"
+SELECT title FROM Movies WHERE title LIKE "WALL%"
